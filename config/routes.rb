b078e30f4d9b374837feeb32626d250c68697d5a
Dan Hossom <dan@Dans-MacBook-Pro-2.local>
2013-12-29 18:34:38 -0800

    generate User model via devise; migrate; test views"
    "

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Tatu::Application.routes.draw do
 
-  get "welcome/index"
   root "welcome#index"
+
+
+  devise_for :users
+  get "welcome/index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
