931e00f639ca4785d4a3054af19381beb7b20dc0
Dan Hossom <dan@Dans-MacBook-Pro-2.local>
2014-01-12 19:04:56 -0800

    edit callback controller function (mistakenly had github data specified)

--- app/controllers/omniauth_callbacks_controller_controller.rb
+++ app/controllers/omniauth_callbacks_controller_controller.rb
@@ -8,7 +8,7 @@ class OmniauthCallbacksControllerController < Devise::OmniauthCallbacksControlle
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Github"
       sign_in_and_redirect user, :event => :authentication
     else
-      session["devise.github_data"] = request.env["omniauth.auth"]
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
