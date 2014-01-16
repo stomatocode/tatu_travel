72a8582bd81a7f768e6fd85c243557a7addef7cd
Dan Hossom <dan@Dans-MacBook-Pro-2.local>
2014-01-16 13:36:14 -0800

    add show method for #welcome controller; use params[:id]

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
 
   before_filter :authenticate_user!
 
-  # def index
-
-  # end
+  def show
+    @user = User.find_by_id(params[:id])
+  end
   
 end
